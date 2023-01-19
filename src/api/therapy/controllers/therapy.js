f425bd313fcca1c2c81070660967cbd91baf7ea0
Lennart Klein <lennart.klein@eo.nl>
2023-01-19 19:50:16 +0100

    remove Therapy, Coaching and rename Membership label

--- src/api/therapy/controllers/therapy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * therapy controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::therapy.therapy');
--- src/api/therapy/services/therapy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * therapy service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::therapy.therapy');
